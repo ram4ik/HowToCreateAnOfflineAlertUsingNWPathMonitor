917f2d847c4aee00f14f50ad8ef19ee97f8568eb
ribragimov-mwb <ribragimov@malwarebytes.com>
2024-06-09 13:57:06 +0300

    Initial Commit

---
+++ HowToCreateAnOfflineAlertUsingNWPathMonitor/HowToCreateAnOfflineAlertUsingNWPathMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToCreateAnOfflineAlertUsingNWPathMonitorApp.swift
+//  HowToCreateAnOfflineAlertUsingNWPathMonitor
+//
+//  Created by Ramill Ibragimov on 6/9/24.
+//
+
+import SwiftUI
+
+@main
+struct HowToCreateAnOfflineAlertUsingNWPathMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
